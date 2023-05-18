2691bafc39f0577cfdcf64d950d4958e46cf73e0
mamartSola <maria.martiyanova@comtech.com>
2023-05-18 16:33:26 -0400

    super base

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,14 +1,2 @@
-create table "postgres"."public"."testing_straight_local_pull"
-as (
-    select
-        _airbyte_emitted_at,
-        (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
-
-        cast(jsonb_extract_path_text("_airbyte_data",'this') as varchar) as "key",
-        cast(jsonb_extract_path_text("_airbyte_data",' is') as varchar) as "date1",
-        cast(jsonb_extract_path_text("_airbyte_data",' a') as varchar) as "date2"
-
-    from "postgres".public._airbyte_raw_testing_straight_local_pull
-);
-
-select * from 
+{{ config(materialized='table') }}
+select * from "postgres".public._airbyte_raw_testing_straight_local_pull
