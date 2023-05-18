d945e5f50f8aba459d7cfd86e38866ff23361145
mamartSola <maria.martiyanova@comtech.com>
2023-05-18 17:17:18 -0400

    again

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,21 +1,19 @@
-create table "postgres".public."night"
+{{ config(materialized='table') }}
 
-  as (
+with a_night_walk as (
 
-    with a_night_walk as (
+    select 
 
-        select 
+        _airbyte_emitted_at,
+        cast(jsonb_extract_path_text("_airbyte_data",'ni') as varchar) as "key"
 
-            _airbyte_emitted_at,
-            cast(jsonb_extract_path_text("_airbyte_data",'ni') as varchar) as "key"
-
-        from "postgres".public._airbyte_raw_night
+    from "postgres".public._airbyte_raw_night
         
-    )
+)
 
 select
 
     _airbyte_emitted_at,
     "key"
 
-from a_nightly walk );
+from a_nightly walk 
