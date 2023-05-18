485bb2c2d32109088f851cb259866973657e2427
mamartSola <maria.martiyanova@comtech.com>
2023-05-18 16:55:34 -0400

    edit source

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -2,6 +2,6 @@
 select 
 
     _airbyte_emitted_at,
-    cast(jsonb_extract_path_text("_airbyte_data",'this') as varchar) as "key"
+    cast(jsonb_extract_path_text("_airbyte_data",'ni') as varchar) as "key"
 
-from "postgres".public._airbyte_raw_testing_straight_local_pull
+from "postgres".public._airbyte_raw_night
