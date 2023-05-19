9e080611b679bc61f45d54d7668031a3d978e56b
mamartSola <maria.martiyanova@comtech.com>
2023-05-19 16:02:57 -0400

    maywork?

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,19 +1,5 @@
-with source as (
+select
+    gh as order_id,
+    ty as customer_id,
 
-    select * from public._airbyte_raw_cafe
-
-),
-
-renamed as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from source
-
-)
-
-select * from renamed
+from night
