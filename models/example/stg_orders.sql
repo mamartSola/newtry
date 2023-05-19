73d51b6151cf7b98a381f58d32c2caf0b9df7661
mamartSola <maria.martiyanova@comtech.com>
2023-05-19 11:28:55 -0400

    attempt

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,2 +1,19 @@
-{{ config(materialized='view') }}
-select * from public._airbyte_raw_night
+with source as (
+
+    select * from public._airbyte_raw_cafe
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from source
+
+)
+
+select * from renamed
