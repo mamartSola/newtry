ba3b6c41de3623938185a22ff7d14eacdae3a3b3
mamartSola <maria.martiyanova@comtech.com>
2023-05-23 09:13:43 -0400

    stripped extra phase 1

--- models/example/stg_orders.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='view') }}
-
-select -- what we pick out from the normalization
-    gh as order_id,
-    ty as customer_id
-
-from public.night -- whats created automatcally, with normalization
