263de8019bda6d28c7ab4210121417e47011b21d
mamartSola <maria.martiyanova@comtech.com>
2023-05-23 09:28:47 -0400

    no normalization with json obj in csv

--- models/example/rec_in.sql
+++ models/example/rec_in.sql
@@ -5,4 +5,4 @@ select -- what we pick out from the normalization
     cast(json_extract_path_text("_airbyte_data"::json,'id') as varchar) as "identification",
     cast(json_extract_path_text("_airbyte_data"::json,'col1') as varchar) as "column"
 
-from public.pullusers -- whats created automatcally, with normalization
+from public._airbyte_raw_pullusers -- whats created automatcally, with normalization
