7ee91a371d02ac7ecdbbc51d92847a3d08feabae
mamartSola <maria.martiyanova@comtech.com>
2023-05-23 09:44:21 -0400

    nested solved

--- models/example/rec_in.sql
+++ models/example/rec_in.sql
@@ -1,8 +1,25 @@
 {{ config(materialized='view') }}
 
-select -- what we pick out from the normalization
-    
-    cast(json_extract_path_text("_airbyte_data"::json,'id') as varchar) as "identification",
-    cast(json_extract_path_text("_airbyte_data"::json,'col1') as varchar) as "column"
 
-from public._airbyte_raw_pullusers -- whats created automatcally, with normalization
+with basics as (
+
+    select -- what we pick out from the normalization
+
+        cast(json_extract_path_text("_airbyte_data"::json,'_airbyte_ab_id') as varchar) as "uid3",
+        cast(json_extract_path_text("_airbyte_data"::json,'_airbyte_data') as varchar) as "thisobj"
+
+    from public._airbyte_raw_pullusers -- whats created automatcally, with normalization
+),
+
+pluck as(
+
+    select
+
+        uid3,
+        cast(json_extract_path_text("thisobj"::json,'id') as varchar) as "objid",
+        cast(json_extract_path_text("thisobj"::json,'col1') as varchar) as "column"
+
+    from basics
+)
+
+select * from pluck
